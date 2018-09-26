b2b50b4a09bdf0a8adcfba875af5c867399e32fa
yondo-r <yondo.ricardo@gmail.com>
2018-09-26 15:16:23 -0300

    comecando a implementar metodo de imprimir tabuleiro

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using tabuleiro;
 
-namespace ConsoleApp1
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
