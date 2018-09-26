b49991460023b7998b4dce11b0b638be7ba868df
yondo-r <yondo.ricardo@gmail.com>
2018-09-26 15:01:00 -0300

    comeco de implementacao de tabuleiro

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace ConsoleApp1
 {
@@ -7,9 +7,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
