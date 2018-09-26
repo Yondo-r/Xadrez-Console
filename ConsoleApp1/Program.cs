2f792eb21e1fe01b3c319b61f38517b631f6e422
yondo-r <yondo.ricardo@gmail.com>
2018-09-26 13:33:36 -0300

    criacao da classe posicao com linha e coluna

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
         }
     }
 }
