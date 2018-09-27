bbcb2ea0fbf8d8a8afddb3cac6fa694aaf884386
yondo-r <yondo.ricardo@gmail.com>
2018-09-27 16:58:14 -0300

    implementando as restricoes de movimento

--- ConsoleApp1/Xadrez/Rei.cs
+++ ConsoleApp1/Xadrez/Rei.cs
@@ -13,5 +13,73 @@ namespace xadrez
         {
             return "R";
         }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool [,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Norte
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //Norte-leste
+            pos.definirValores(posicao.linha-1, posicao.coluna +1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            //Leste
+            pos.definirValores(posicao.linha, posicao.coluna +1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //Sul-leste
+            pos.definirValores(posicao.linha + 1, posicao.coluna+1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //Sul
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //Sul-oeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna -1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //Oeste
+            pos.definirValores(posicao.linha, posicao.coluna -1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //Norte-oeste
+            pos.definirValores(posicao.linha -1, posicao.coluna-1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            return mat;
+        }
     }
 }
